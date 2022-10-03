97361c3d11eab4860ff6c7d099346908f9c2cbe3
Yuri Pessoal <yuri.silva@credpago.com>
2022-10-03 19:02:29 -0300

    feat: Prisma database teardown logic

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,11 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common'
 import { Test } from '@nestjs/testing'
+import { PrismaService } from 'src/prisma/prisma.service'
 import { AppModule } from '../src/app.module'
 
 describe('App e2e', ()=> {
   let app: INestApplication
+  let prisma: PrismaService
 
   beforeAll(async ()=> {
     const moduleRef = await Test.createTestingModule({
@@ -19,10 +21,55 @@ describe('App e2e', ()=> {
     )
 
     await app.init()
+
+    prisma = app.get(PrismaService)
+
+    await prisma.cleanDb()
   })
 
   afterAll(()=> {
     app.close()
   })
-  it.todo('should pass')
+  
+  describe('Auth', ()=> {
+    describe('Signup', ()=>{
+      it.todo('should signup')
+    })
+
+    describe('Signin', ()=>{
+      it.todo('should signin')
+    })
+  })
+
+  describe('User', ()=> {
+    describe('Get', ()=>{
+      
+    })
+
+    describe('Edit', ()=>{
+      
+    })
+  })
+
+  describe('Bookmarks', ()=> {
+    describe('Create', ()=>{
+      
+    })
+
+    describe('Get', ()=>{
+      
+    })
+
+    describe('Get by id', ()=>{
+      
+    })
+
+    describe('Edit', ()=>{
+      
+    })
+
+    describe('Delete', ()=>{
+      
+    })
+  })
 })
