2f9fc97cf803841602222d75474de35659f7fe62
Yuri Pessoal <yuri.silva@credpago.com>
2022-10-03 15:51:05 -0300

    feat: adding e2e tests with pactumJs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,6 @@
 import { Body, Controller, HttpCode, HttpStatus, Post, Req } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthDto } from "./dto";
-
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService){}
