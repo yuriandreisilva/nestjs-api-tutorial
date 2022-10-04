906dd8c5e55c1ff32683cf6e842da13e900992ba
Yuri Pessoal <yuri.silva@credpago.com>
2022-10-04 09:32:22 -0300

    feat: auth tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,8 @@ import { INestApplication, ValidationPipe } from '@nestjs/common'
 import { Test } from '@nestjs/testing'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { AppModule } from '../src/app.module'
+import * as pactum from 'pactum'
+import { AuthDto } from 'src/auth/dto'
 
 describe('App e2e', ()=> {
   let app: INestApplication
@@ -21,10 +23,12 @@ describe('App e2e', ()=> {
     )
 
     await app.init()
+    await app.listen(3333)
 
     prisma = app.get(PrismaService)
 
     await prisma.cleanDb()
+    pactum.request.setBaseUrl('http://localhost:3333')
   })
 
   afterAll(()=> {
@@ -32,12 +36,91 @@ describe('App e2e', ()=> {
   })
   
   describe('Auth', ()=> {
+    const dto: AuthDto = {
+      email: 'yuri@gmail.com',
+      password: '123'
+    }
     describe('Signup', ()=>{
-      it.todo('should signup')
+      it('should throw if email empty', () =>{
+        return pactum
+          .spec()
+          .post(
+            '/auth/signup'
+          ).withBody({
+            password: dto.password
+          })
+          .expectStatus(400)
+          .inspect()
+      })
+      it('should throw if password empty', () =>{
+        return pactum
+          .spec()
+          .post(
+            '/auth/signup'
+          ).withBody({
+            email: dto.email
+          })
+          .expectStatus(400)
+          .inspect()
+      })
+      it('should throw if no body provided', () =>{
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .expectStatus(400)
+          .inspect()
+      })
+      it('should signup', ()=>{
+        return pactum
+          .spec()
+          .post(
+            '/auth/signup'
+          ).withBody(dto)
+          .expectStatus(201)
+          .inspect()
+      })
     })
-
     describe('Signin', ()=>{
-      it.todo('should signin')
+      let accessToken: string
+
+      it('should throw if email empty', () =>{
+        return pactum
+          .spec()
+          .post(
+            '/auth/signin'
+          ).withBody({
+            password: dto.password
+          })
+          .expectStatus(400)
+          .inspect()
+      })
+      it('should throw if password empty', () =>{
+        return pactum
+          .spec()
+          .post(
+            '/auth/signin'
+          ).withBody({
+            email: dto.email
+          })
+          .expectStatus(400)
+          .inspect()
+      })
+      it('should throw if no body provided', () =>{
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .expectStatus(400)
+          .inspect()
+      })
+      it('should signin', () =>{
+        return pactum
+          .spec()
+          .post(
+            '/auth/signin'
+          ).withBody(dto)
+          .expectStatus(200)
+          .stores('userAt', 'access_token')
+      })
     })
   })
 
